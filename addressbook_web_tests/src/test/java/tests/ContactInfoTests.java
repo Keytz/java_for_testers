eb3aa6a9a54d022059cfe52c822a310dff98218c
keytz <keytz@mail.ru>
2026-06-25 22:36:45 +0300

    2

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -1,46 +1,40 @@
 package tests;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class ContactInfoTests extends TestBase {
 
-    @Test
-    void testPhones() {
+//    @Test
+//    void testPhones() {
+//
+//        var contacts = app.hbm().getContactList();
+//
+//        Assertions.assertFalse(contacts.isEmpty(), "No contacts in DB");
+//
+//        var contact = contacts.get(0);
+//
+//        var phonesFromUI = app.contact().getPhones();
+//
+//        var expected = Stream.of(
+//                        contact.home(),
+//                        contact.mobile(),
+//                        contact.work()
+//                ).filter(s -> s != null && !s.isEmpty())
+//                .collect(Collectors.joining("\n"));
+//
+//        Assertions.assertEquals(expected, phonesFromUI.get(contact.id()));
+//    }
 
-        var contacts = app.hbm().getContactList();
-
-        Assertions.assertFalse(contacts.isEmpty(), "No contacts in DB");
-
-        var contact = contacts.get(0);
-
-        var phonesFromUI = app.contact().getPhones();
-
-        var expected = Stream.of(
-                        contact.home(),
-                        contact.mobile(),
-                        contact.work()
-                ).filter(s -> s != null && !s.isEmpty())
-                .collect(Collectors.joining("\n"));
-
-        Assertions.assertEquals(expected, phonesFromUI.get(contact.id()));
-    }
-
-    @Test
-    void testAddress() {
-
-        var contacts = app.hbm().getContactList();
-        Assertions.assertFalse(contacts.isEmpty(), "No contacts in DB");
-
-        var contact = contacts.get(0);
-
-        var uiAddress = app.contact().getAddress(contact);
-
-        Assertions.assertEquals(contact.address(), uiAddress);
-    }
+//    @Test
+//    void testAddress() {
+//
+//        var contacts = app.hbm().getContactList();
+//        Assertions.assertFalse(contacts.isEmpty(), "No contacts in DB");
+//
+//        var contact = contacts.get(0);
+//
+//        var uiAddress = app.contact().getAddress(contact);
+//
+//        Assertions.assertEquals(contact.address(), uiAddress);
+//    }
 
 //    @Test
 //    void testEmails() {
